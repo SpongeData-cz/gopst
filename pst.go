2e83aa003b7f0483b749d8b6d41ec6258cd05dfd
Adela Reslerova <adela.reslerova94@gmail.com>
2023-09-11 09:37:16 +0200

    shortening GetDir

--- pst.go
+++ pst.go
@@ -229,10 +229,7 @@ type Record struct {
 Predicate - is directory?
 */
 func (ego *Record) GetDir() bool {
-	if typeOfRcrd := uint8(ego.record._type); typeOfRcrd == 4 {
-		return true
-	}
-	return false
+	return uint8(ego.record._type) == 4
 }
 
 /*
